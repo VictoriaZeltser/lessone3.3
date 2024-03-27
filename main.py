41a1c8250549fb26f03c3c5885dd2c809fc5e5c7
VictoriaZeltser <vikaz1976@gmail.com>
2024-03-28 00:04:59 +0200

    add  winner

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/4264.jpeg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png.png")
 target_width = 80
 target_height = 80
 
@@ -19,6 +19,8 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+score = 0
 
 
 running = True
@@ -29,9 +31,14 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                score += 1  # Увеличиваем счет на 1
+                pygame.display.set_caption(f"Игра Тир - Очки: {score}")  # Обновляем заголовок окна с новым счетом
+
+          
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
 
